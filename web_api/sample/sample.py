c4bba59c226ad65501920eda838ec0b2858eb3b7
John Rose <johnedmundrose@gmail.com>
2023-09-25 14:28:19 -0400

    WebAPI sample app

---
+++ web_api/sample/sample.py
@@ -0,0 +1,37 @@
+# This is the sample from https://fastapi.tiangolo.com/
+
+from typing import Union
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Category(BaseModel):
+    category_id: int
+    name: str
+    description: str
+
+
+class Item(BaseModel):
+    item_id: int
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+    category: Category
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
